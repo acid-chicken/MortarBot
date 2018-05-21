f1baf67c484c37905738fdc2ba80b1da2bd3cc7a
Acid Chicken (硫酸鶏) <root@acid-chicken.com>
2018-05-22 00:26:42 +0900

    Create project

---
+++ src/MortarBot/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace MortarBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
